c444ecea58e2b42a78534811c0217fa1e4169903
AmirHosein <awir.software@gmail.com>
2023-04-04 14:17:22 +0430

    add product model

---
+++ src/Model/products/Product.model.js
@@ -0,0 +1,12 @@
+export const Product = [
+    {
+        id: 123,
+        description: 'Red Shoe',
+        price: 12.22
+    },
+    {
+        id: 431,
+        description: 'Blue Shoe',
+        price: 16.99
+    },
+]
